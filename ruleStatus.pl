79005d419541170f4e94016f403dc0f5b8c10408
tastytypist <nathanael310802@gmail.com>
2021-11-28 14:26:29 +0700

    Add current time and day to be displayed

--- ruleStatus.pl
+++ ruleStatus.pl
@@ -1,33 +1,41 @@
-% digunakan untuk menampilkan status
+% File ruleStatus.pl
+% Contributor : 13520042
 
-% include fakta global
+% Include global facts
 :- include('globalFact.pl').
 
 % Facts for debugging purposes
-% level(5).
-% class('Farmer').
-% levelFarming(12).
-% levelFishing(8).
-% levelRanching(10).
-% expFarming(6628).
-% expFishing(3877).
-% expRanching(4652).
-% gold(12000).
-% exp(13864).
+%currentTime('12.00').
+%currentDay('Thursday').
+%level(5).
+%class('Farmer').
+%levelFarming(12).
+%levelFishing(8).
+%levelRanching(10).
+%expFarming(6628).
+%expFishing(3877).
+%expRanching(4652).
+%gold(12000).
+%exp(13864).
 
-% Getting facts' value
-gold(Gold).
-class(Class).
-level(Level).
-exp(Exp).
-levelFarming(LevelFarming).
-expFarming(ExpFarming).
-levelFishing(LevelFishing).
-expFishing(ExpFishing).
-levelRanching(LevelRanching).
-expRanching(ExpRanching).
+status :-
+    % Get latest data facts from global facts
+    currentTime(Time),
+    currentDay(Day),
+    gold(Gold),
+    class(Class),
+    level(Level),
+    exp(Exp),
+    levelFarming(LevelFarming),
+    expFarming(ExpFarming),
+    levelFishing(LevelFishing),
+    expFishing(ExpFishing),
+    levelRanching(LevelRanching),
+    expRanching(ExpRanching),
 
-status :- 
+    % Displays facts on screen
+    write('Day: '), write(Day), nl,
+    write('Time: '), write(Time), nl, nl,
     write('Your status:'), nl,
     write('Gold: '), write(Gold), nl,
     write('Job: '), write(Class), nl,
