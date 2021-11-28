8cd0323254aced1bbf2db854d9370a3abfeb919a
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2021-11-29 02:32:13 +0700

    Update ruleFishing2 -> ruleFishing.pl

--- ruleFishing.pl
+++ ruleFishing.pl
@@ -1,108 +1,237 @@
 :- include('globalFact.pl').
 :- include('globalRule.pl').
 
-:- dynamic().
-
-
-fishing:-
+fish:-
     playerLoc(X,Y),
     X1 is X-1,
     waterLoc(X1,Y),
-    random(1,8,Z)  
-    goFishing(Z),!.
+    random(0,8,Z),
+    currentTime(T),
+    goFishing,!.
 
-fishing:-
+fish:-
     playerLoc(X,Y),
     X1 is X+1,
     waterLoc(X1,Y),
-    random(1,8,Z)  
-    goFishing(Z),!.
-
-fishing:-
+    random(0,8,Z),
+    currentTime(T),
+    goFishing,!.
+fish:-  
     playerLoc(X,Y),
     Y1 is Y-1,
     waterLoc(X,Y1),
-    random(1,8,Z)  
-    goFishing(Z),!.
+    random(0,8,Z),
+    currentTime(T),
+    goFishing,!.
 
-fishing:-
+fish:-
     playerLoc(X,Y),
     Y1 is Y+1,
     waterLoc(X,Y1),
-    random(1,8,Z),
-    goFishing(Z),!.
+    random(0,8,Z),
+    currentTime(T),
+    goFishing,!.
+
+fish:-
+    write('Maaf kamu tidak berada di dekat air, Pergi ke tempat air untuk memancing!!!'),
+    nl,
+    !.
 
 goFishing(X):-
-    levelFishing(1),
-    1 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == tuna,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
+    findItem(ItName, TheItem),
+    TheItem \== [], TheItem  = [TheItemName, TheItemCount, _], 
+    TheItemCount <1,
+    write('Maaf kamu ga punya fishing rod!!'),nl,
+    write('Beli di Market dulu'),nl,
+    !.
 
 goFishing(X):-
-    levelFishing(1),
-    0 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == salmon,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
+    levelFishing(_),
+    totalItemCount(Total),
+    Total=:=100,
+    write('Item penuh, kosongkan inventory terlebih dahulu!'),
+    nl,!.
+
+%nilai 0-3 Zonk, 4-6 Tuna, 7-8 Salmon, 9=< catfish
+goFishing(X):-
+    class(fisherman),
+    levelFishing(Z),
+    X1 is X+Z,
+    X1 < 4,
+    random(2,5,J),
+    addTimeByX(J),
+    write('Sepertinya ikanmu kabur, kamu kurang beruntung!'),
+    nl,!.
+
+goFishing(X):-
+    class(fisherman),
+    levelFishing(Z),
+    Z>=1,
+    Z<=3,
+    X1 is X+Z,
+    X1 >= 4,
+    X1<=6,
+    random(1,3,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Tuna!!'),nl,
+    write(' __v_'),nl,
+    write('(____ /{'),nl,
+    Z1 is Z+15,
+    asserta(expFishing(Z1)), 
+    changeItemCount('tuna',1),
+    asserta(levelFishing(Y1)),!.
+
+goFishing(X):-
+    class(fisherman),
+    levelFishing(L),
+    X1 is X+3,
+    X1>=7,
+    X1<9,
+    random(2,4,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Salmon!!'),
+    nl,
+    write('        /\\ '),nl,
+    write('      _/./  '),nl,
+    write('   ,-|    `-:.,-/ '),nl
+    write('  > O )<)    _  ('),nl,
+    write('  `-._  _.:\' `-.\\ '),nl,
+    write('     `` \\ '),nl,
+    Z1 is Z+30,
+    asserta(expFishing(Z1)), 
+    changeItemCount('salmon',1),
+    asserta(levelFishing(Y1)),!.
+
+goFishing(X):-
+    class(fisherman),
+    levelFishing(L),
+    L>=3,
+    X1 is X+3,
+    X1>=9,
+    random(3,5,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Catfish!!'),
+    nl,
+    write('          /"*._         _'),nl,
+    write('      .-*'`    `*-.._.-'/'),nl,
+    write('    < * ))     ,       ( '),nl,
+    write('      `*-._`._(__.--*"`.\\ '),nl,
+    retractall(expFishing(Z)),
+    Z1 is Z+40,
+    asserta(expFishing(Z1)),   
+    changeItemCount('catfish',1),
+    asserta(levelFishing(Y1)),!.
+
+
+
+%Memancing jika dia seorang fishbiasaer
+goFishing(X):-
+    levelFishing(Y),
+    Y>3,
+    X1 is X+Y,
+    X1 >=4,
+    X1<=6,
+    random(2,3,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Tuna!!'),nl,
+    write(' __v_'),nl,
+    write('(____ /{'),nl,
+    Z1 is Z+10,
+    asserta(expFishing(Z1)), 
+    changeItemCount('tuna',1),
+    asserta(levelFishing(Y1)),!.
+
+goFishing(X):-
+    levelFishing(Y),
+    Y>3,
+    X1 is X+Y,
+    X1>6,
+    X1<9,
+    random(2,4,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Salmon!!'),
+    nl,
+    write('        /\\ '),nl,
+    write('      _/./  '),nl,
+    write('   ,-|    `-:.,-/ '),nl
+    write('  > O )<)    _  ('),nl,
+    write('  `-._  _.:\' `-.\\ '),nl,
+    write('     `` \\ '),nl,
+    Z1 is Z+25,
+    asserta(expFishing(Z1)), 
+    changeItemCount('salmon',1),
+    asserta(levelFishing(Y1)),!.
+
+goFishing(X):-
+    levelFishing(Y),
+    Y>3,
+    X1 is X+Y,
+    X1>=9,
+    random(3,6,J),
+    addTimeByX(J),
+    write('Selamat kamu berhasil memancing sebuah Catfish!!'),
+    nl,
+    write('          /"*._         _'),nl,
+    write('      .-*'`    `*-.._.-'/'),nl,
+    write('    < * ))     ,       ( '),nl,
+    write('      `*-._`._(__.--*"`.\\ '),nl,
+    retractall(expFishing(Z)),
+    Z1 is Z+35,
+    asserta(expFishing(Z1)),   
+    changeItemCount('catfish',1),
+    asserta(levelFishing(Y1)),!.
+
+%Jika dia orang lain
 
 
 goFishing(X):-
-    levelFishing(2),
-    0 is X mod 3,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == catfish,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
-
-
-goFishing(X):-
-    levelFishing(2),
-    1 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == tuna,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
+    levelFishing(Y),
+    X1 is X+Y,
+    X1 >=4,
+    X1<=6,
+    addTimeByX32),
+    write('Selamat kamu berhasil memancing sebuah Tuna!!'),nl,
+    write(' __v_'),nl,
+    write('(____\ /{'),nl,
+    Z1 is Z+5,
+    asserta(expFishing(Z1)), 
+    changeItemCount('tuna',1),
+    asserta(levelFishing(Y1)),!.
 
 goFishing(X):-
-    levelFishing(2),
-    0 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == salmon,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
+    levelFishing(Y),
+    X1 is X+Y,
+    X1>6,
+    X1<9,
+    addTimeByX(4),
+    write('Selamat kamu berhasil memancing sebuah Tuna!!'),
+    nl,
+    write('        /\\ '),nl,
+    write('      _/./  '),nl,
+    write('   ,-|    `-:.,-/ '),nl
+    write('  > O )<)    _  ('),nl,
+    write('  `-._  _.:\' `-.\\ '),nl,
+    write('     `` \\ '),nl,
+    Z1 is Z+15,
+    asserta(expFishing(Z1)), 
+    changeItemCount('salmon',1),
+    asserta(levelFishing(Y1)),!.
 
 goFishing(X):-
-    levelFishing(3),
-    0 is X mod 3,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == catfish,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
+    levelFishing(Y),
+    X1 is X+Y,
+    X1>=9,
+    addTimeByX(5),
+    write('Selamat kamu berhasil memancing sebuah Tuna!!'),
+    nl,
+    write('          /"*._         _'),nl,
+    write('      .-*'`    `*-.._.-'/'),nl,
+    write('    < * ))     ,       ( '),nl,
+    write('      `*-._`._(__.--*"`.\\ '),nl,
+    retractall(expFishing(Z)),
+    Z1 is Z+30,
+    asserta(expFishing(Z1)),   
+    changeItemCount('catfish',1),
+    asserta(levelFishing(Y1)),!.
 
 
-goFishing(X):-
-    levelFishing(3),
-    1 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == tuna,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
-
-goFishing(X):-
-    levelFishing(3),
-    0 is X mod 2,
-    inventoryList([NamaIkan JumlahIkan Level]),
-    NamaIkan == salmon,
-    JumlahBaru is JumlahIkan+1,
-    retract(inventoryList([NamaIkan JumlahIkan Level])),
-    aserta(inventoryList([NamaIkan JumlahBaru Level])),!.
