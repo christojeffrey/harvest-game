8481ff07ce1e033f0eea5900f633a1b2f73c98c2
LordGedelicious <gede.coolguy@gmail.com>
2021-11-29 12:28:52 +0700

    FIX BANGET RULERANCH.PL

--- ruleRanch.pl
+++ ruleRanch.pl
@@ -59,6 +59,7 @@ ranch :-
     ((X1 =:= X2, Y1 =:= Y2, showRanch); ((X1 =\= X2; Y1 =\= Y2), notRanch)).
 
 showRanch :-
+    isCommandAllowed,
     write('Selamat datang di Ranch!\n'),
     write('Di peternakan anda, anda memiliki:\n'),
     findItem('cow', CowCountLevel),
@@ -75,18 +76,21 @@ showRanch :-
 
 doAction(Action, CowQty, SheepQty, ChickenQty) :-
     bonusEXPRanchClass(BonusEXP),
+    totalItemCount(TotalCount),
     (   Action = 'cow', 
         ((CowQty > 0, cowCooldown(CowTime), (
             (
                 CowTime = 0, 
                 ((CowQty < 5, cowMaxProduce(CowQty, MaxMilk),
                 random(0, MaxMilk, MilkProduce),
-                changeItemCount('milk', MilkProduce),
+                TempCount is TotalCount + MilkProduce,
                 (
                     (
                         MilkProduce =:= 0, write('Yah sapi kamu belum ada yang menghasilkan susu. Tunggu nanti ya!\n')
                     );(
-                        MilkProduce > 0, write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu!\n')
+                        MilkProduce > 0, TempCount =< 100, write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu!\n'), changeItemCount('milk', MilkProduce)
+                    );(
+                        MilkProduce > 0, TempCount > 100 , MilkProduce2 is TempCount - 100, write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(MilkProduce2), write(' botol susu!\n'), changeItemCount('milk', MilkProduce2)
                     )
                 ),
                 refreshCowCooldown, addTimeByX(1),
@@ -99,12 +103,14 @@ doAction(Action, CowQty, SheepQty, ChickenQty) :-
                 ExtraCow is CowQty - 4,
                 MaxMilk is MaxMilk + ExtraCow,
                 random(0, MaxMilk, MilkProduce),
-                changeItemCount('milk', MilkProduce),
+                TempCount is TotalCount + MilkProduce,
                 (
                     (
                         MilkProduce =:= 0, write('Yah sapi kamu belum ada yang menghasilkan susu. Tunggu nanti ya!\n')
                     );(
-                        MilkProduce > 0, write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu!\n')
+                        MilkProduce > 0, TempCount =< 100 , write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu!\n'), changeItemCount('milk', MilkProduce)
+                    );(
+                        MilkProduce > 0, TempCount > 100 , MilkProduce2 is TempCount - 100, write('Sapi kamu menghasilkan '), write(MilkProduce), write(' botol susu, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(MilkProduce2), write(' botol susu!\n'), changeItemCount('milk', MilkProduce2)
                     )
                 ),
                 refreshCowCooldown, addTimeByX(1),
@@ -130,12 +136,14 @@ doAction(Action, CowQty, SheepQty, ChickenQty) :-
                 SheepTime = 0, 
                 ((SheepQty < 5, chickenMaxProduce(SheepQty, MaxWool),
                 random(0, MaxWool, WoolProduce),
-                changeItemCount('wool', WoolProduce),
+                TempCount is TotalCount + WoolProduce,
                 (
                     (
                         WoolProduce =:= 0, write('Yah domba kamu belum ada yang menghasilkan wol. Tunggu nanti ya!\n')
                     );(
-                        WoolProduce > 0, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol!\n')
+                        WoolProduce > 0, TempCount =< 100, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol!\n'), changeItemCount('wool', WoolProduce)
+                    );(
+                        WoolProduce > 0, TempCount > 100 , WoolProduce2 is TempCount - 100, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(WoolProduce2), write(' gulung wol!\n'), changeItemCount('wool', WoolProduce2)
                     )
                 ),
                 refreshSheepCooldown, addTimeByX(1),
@@ -147,12 +155,14 @@ doAction(Action, CowQty, SheepQty, ChickenQty) :-
                 ExtraSheep is SheepQty - 4,
                 MaxWool is ExtraSheep + MaxWool,
                 random(0, MaxWool, WoolProduce),
-                changeItemCount('wool', WoolProduce),
+                TempCount is TotalCount + WoolProduce,
                 (
                     (
                         WoolProduce =:= 0, write('Yah domba kamu belum ada yang menghasilkan wol. Tunggu nanti ya!\n')
                     );(
-                        WoolProduce > 0, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol!\n')
+                        WoolProduce > 0, TempCount =< 100, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol!\n'), changeItemCount('wool', WoolProduce)
+                    );(
+                        WoolProduce > 0, TempCount > 100 , WoolProduce2 is TempCount - 100, write('Domba kamu menghasilkan '), write(WoolProduce), write(' gulung wol, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(WoolProduce2), write(' gulung wol!\n'), changeItemCount('wool', WoolProduce2)
                     )
                 ),
                 refreshSheepCooldown, addTimeByX(1),
@@ -176,12 +186,14 @@ doAction(Action, CowQty, SheepQty, ChickenQty) :-
                 ChickenTime = 0, 
                 ((ChickenQty < 5, chickenMaxProduce(ChickenQty, MaxEgg),
                 random(0, MaxEgg, EggProduce),
-                changeItemCount('egg', EggProduce),
+                TempCount is TotalCount + EggProduce,
                 (
                     (
                         EggProduce =:= 0, write('Yah ayam kamu belum ada yang menghasilkan telur. Tunggu nanti ya!\n')
                     );(
-                        EggProduce > 0, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur!\n')
+                        EggProduce > 0, TempCount =< 100, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur!\n'), changeItemCount('egg', EggProduce)
+                    );(
+                        EggProduce > 0, TempCount > 100 , EggProduce2 is TempCount - 100, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(EggProduce2), write(' butir telur!\n'), changeItemCount('egg', EggProduce2)
                     )
                 ),
                 refreshChickenCooldown, addTimeByX(1),
@@ -194,12 +206,14 @@ doAction(Action, CowQty, SheepQty, ChickenQty) :-
                 ExtraChicken is ChickenQty - 4,
                 MaxEgg is MaxEgg + ExtraChicken,
                 random(0, MaxEgg, EggProduce),
-                changeItemCount('egg', EggProduce),
+                TempCount is TotalCount + EggProduce,
                 (
                     (
                         EggProduce =:= 0, write('Yah ayam kamu belum ada yang menghasilkan telur. Tunggu nanti ya!\n')
                     );(
-                        EggProduce > 0, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur!\n')
+                        EggProduce > 0, TempCount =< 100, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur!\n'), changeItemCount('egg', EggProduce)
+                    );(
+                        EggProduce > 0, TempCount > 100 , EggProduce2 is TempCount - 100, write('Ayam kamu menghasilkan '), write(EggProduce), write(' butir telur, tapi karena inventory kamu penuh, kamu cuma bisa menyimpan '), write(EggProduce2), write(' butir telur!\n'), changeItemCount('egg', EggProduce2)
                     )
                 ),
                 refreshChickenCooldown, addTimeByX(1),
